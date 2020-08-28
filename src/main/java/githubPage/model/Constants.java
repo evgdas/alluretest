30f617795a1af673eeba30fdc2c26da4fc6b62f3
Evgeniy Dashuk <56819251+evgdas@users.noreply.github.com>
2020-08-28 09:59:44 +0200

    Update and rename Constans.java to Constants.java

--- src/main/java/githubPage/model/Constants.java
+++ src/main/java/githubPage/model/Constants.java
@@ -1,6 +1,6 @@
 package githubPage.model;
 
-public class Constans {
+public class Constants {
     public static final String REPOSITORY = "/evgdas/qaguru/";
     public static final String ISSUE_TITLE = "qa guru hello!";
     public static final String LABEL = "bug";
